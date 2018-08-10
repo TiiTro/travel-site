c3b6e2f0401b994e6a682af52dca18970c83ad85
Tiia <tiiatrogen@gmail.com>
2018-08-10 12:41:08 +0300

    Started with webpack, not working

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,6 @@
-// A CONSTRUCTOR-function
-function Person(fullName, favColor) {
-  this.name = fullName;
-  this.favoriteColor = favColor;
-  this.greet = function() {
-    console.log("Hello, my name is " + fullName + " and my favorite color is " + favColor + ".");
-  }
-}
+var Person = require('./modules/Person');
 
-var john = new Person("John Doe", "blue");
+var john = new Person("Kittie Cattie", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
